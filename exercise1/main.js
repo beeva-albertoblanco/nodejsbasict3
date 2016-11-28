9e2f5460ec7873cb0edf2690dd93ab16e338e189
Alvaro Cañada <alvaro.canada@bbva.com>
2016-11-28 22:15:51 +0100

    Finished exercise 1

--- exercise1/main.js
+++ exercise1/main.js
@@ -8,12 +8,29 @@ function listener() {
 	console.log('Coming...');
 }
 
+function shout() {
+    console.log('I SAY I AM COMING');
+}
+
 // TODO Write an interval that emits every second 'ringbell' event with 'ring ring' tone as parameter
-setInterval(() => {
+
+var interval = setInterval(() => {
 	myEventEmitter.emit('ringbell', 'ring ring');
 }, 1000);
 
 // TODO add an event listener with .on() method that listens 'ringbell' event and prints given tone
-	console.log(tone);
+myEventEmitter.on('ringbell', (tone) => {
+    console.log(tone);
+});
 
 //TODO: dd an event listener with .on() method that listens 'ringbell' event and calls listener method
+
+myEventEmitter.on('ringbell', (tone) => {
+    listener();
+});
+
+// Timeout to end events emision and finally answer the door
+setTimeout(() => {
+    clearInterval(interval);
+    shout();
+}, 4000);
